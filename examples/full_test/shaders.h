155ff6725b5bc90c3381fbc70c37b4bad162bac7
jackwthake <jackthake@hotmail.com>
2025-09-22 03:06:59 -0700

    Moved shader code from full_test to its own source file

---
+++ examples/full_test/shaders.h
@@ -0,0 +1,17 @@
+#ifndef FULL_TEST_SHADERS_H
+#define FULL_TEST_SHADERS_H
+
+#include <cpu-render/renderer.h>
+
+// Fragment Shader Functions
+u32 frag_cube_func(u32 input, fragment_context_t context, void *argv, usize argc);
+u32 frag_plane_func(u32 input, fragment_context_t context, void *argv, usize argc);
+u32 frag_sphere_func(u32 input, fragment_context_t context, void *argv, usize argc);
+u32 particle_frag_func(u32 input, fragment_context_t context, void *argv, usize argc);
+
+// Vertex Shader Functions
+float3 plane_ripple_vertex_shader(vertex_context_t context, void *argv, usize argc);
+float3 sphere_blob_vertex_shader(vertex_context_t context, void *argv, usize argc);
+float3 billboard_vertex_shader(vertex_context_t context, void *argv, usize argc);
+
+#endif // FULL_TEST_SHADERS_H
